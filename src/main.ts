be839d44542ffed1652cad9ea055b88a85c87459
jagobainda <jagobainda@gmail.com>
2025-05-15 23:27:54 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+class App {
+  constructor() {
+    this.bindEvents();
+  }
+
+  private bindEvents(): void {
+    
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  new App();
+});
